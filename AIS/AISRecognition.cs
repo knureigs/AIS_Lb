13bc0f0c22b7b3b4b4f829abb5e5c30ac765b39e
Unknown <heorhii.ivashchenko@nure.ua>
2019-10-10 10:18:42 +0300

    Separate namespaces

--- AIS/AISRecognition.cs
+++ AIS/AISRecognition.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace IVS_Lb_1
+namespace AIS
 {
     class AISRecognition
     {
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AIS;
 
 namespace IVS_Lb_1
 {
